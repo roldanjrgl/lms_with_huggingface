76a5769c9162f325f2e581d2b1f04832e43c3f25
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2023-05-09 12:43:59 -0400

    implemented get_model_tokenizer():

--- gpt/gpt2.py
+++ gpt/gpt2.py
@@ -1,5 +1,28 @@
+import os
+
 from transformers import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
 from transformers import pipeline, set_seed
+import os
+
+def get_model_tokenizer():
+    """ Setup model and its tokenizer"""
+    model_dir = "gpt/gpt2_model"
+    checkpoint = "gpt2"
+    if not os.path.exists(model_dir):
+        print(f"Model {model_dir} does not exist. It will be downloaded from Huggingface")
+        os.makedirs(model_dir)
+
+        tokenizer = GPT2Tokenizer.from_pretrained(checkpoint)
+        model = GPT2LMHeadModel.from_pretrained(checkpoint)
+
+        model.save_pretrained(model_dir)
+        tokenizer.save_pretrained(model_dir)
+    else:
+        print(f"Model {model_dir} stored locally. This local version will be uploaded")
+        tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
+        model = GPT2LMHeadModel.from_pretrained(model_dir)
+    return tokenizer, model
+
 
 def gpt2_pipelines():
     generator = pipeline('text-generation', model='gpt2')
@@ -8,15 +31,15 @@ def gpt2_pipelines():
     print(output)
 
 def gpt2_stages():
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    tokenizer, model = get_model_tokenizer()
 
     """ Stage-1: Pre-processing """
-    text = "Replace me by any text you'd like."
+    text = "Today is a beautiful day, I will"
     encoded_input = tokenizer(text, return_tensors='pt')
     # input_ids = encoded_input["input_ids"]
     # attention_mask = encoded_input["attention_mask"]
 
+    """ Stage-2: Model """
     generated_sequence = model.generate(**encoded_input)
     generated_sequence = generated_sequence[0]
     print(generated_sequence)
@@ -27,6 +50,7 @@ def gpt2_stages():
         generated_sequence = generated_sequence.reshape(in_b, out_b // in_b, *generated_sequence.shape[1:])
     generated_sequence = generated_sequence.numpy().tolist()
 
+    """ Stage-3: Postprocessing """
     text = tokenizer.decode(
         generated_sequence[0],
         skip_special_tokens=True,
